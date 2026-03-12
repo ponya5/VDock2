325c29e39b4c10a40fc5a9a2b154adf02af7aafe
ponya5 <ponya81@gmail.com>
2026-03-12 20:20:28 +0200

    chore: update cleanup scripts and maintenance docs

--- remove_temp_files.bat
+++ remove_temp_files.bat
@@ -1,11 +1,1 @@
-@echo off
-REM Remove temporary files
-del scripts\cleanup_files.bat
-del scripts\create-desktop-shortcut.bat
-del scripts\fix_setup.bat
-del scripts\fix_shortcut.bat
-del scripts\install_cross_env.bat
-del scripts\install_electron_deps.bat
-del scripts\launchers\launch.ps1
-del scripts\launchers\launch.sh
-echo Temporary files removed!
+ 
